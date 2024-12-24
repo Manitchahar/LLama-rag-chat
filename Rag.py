3327c5ed73d2d682aef81a63de0347aab0108c11
Manit Kumar <chaharmanit@gmail.com>
2024-12-24 15:16:43 +0530

    rework

--- Rag.py
+++ Rag.py
@@ -9,7 +9,7 @@ import pandas as pd
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 
 # Load environment variables
 load_dotenv()
